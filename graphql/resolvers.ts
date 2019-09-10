64d466d9e8980d5b55d545f266ba1b0f5ba85855
sokphaladam <sokphaladam@gmail.com>
2019-09-10 14:35:21 +0700

    as

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -43,9 +43,9 @@ export const resolvers = {
             }).then(() => `/images/${uuid}${filename}`);
         }
     },
-    Subscription: {
-        setLove: {
-            subscribe: (parent: any, args: any, ctx: any) => ctx.pubsub.asyncIterator('Love')
-        }
-    }
+    // Subscription: {
+    //     setLove: {
+    //         subscribe: (parent: any, args: any, ctx: any) => ctx.pubsub.asyncIterator('Love')
+    //     }
+    // }
 }
