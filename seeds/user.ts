fe2ef33c8b841263b7eb5c7da9601a44c5a9f169
sokphaladam <sokphaladam@gmail.com>
2019-10-11 15:03:42 +0700

    modified:   seeds/user.ts

--- seeds/user.ts
+++ seeds/user.ts
@@ -47,9 +47,41 @@ const bookData = [
     },
     {
         id: 4,
-        title: 'The Moon being round, half of it is lit up by the sun. As it goes around (or orbits) the Earth, sometimes the side that people on Earth can see is all lit brightly. Other times only a small part of the side we see is lit. This is because the Moon does not send out its own light. People only see the parts that are being lit by sunlight. These different stages are called Phases of the Moon.',
-        picture: '[\"http://104.248.156.237:4000/images/A8At6LCZpexels-photo-1413412.jpeg\"]',
+        title: 'City Beautiful movement, American urban-planning movement led by architects, landscape architects, and reformers that flourished between the 1890s and the 1920s. The idea of organized comprehensive urban planning arose in the United States from the City Beautiful movement, which claimed that design could not be separated from social issues and should encourage civic pride and engagement. Its influence was most prominent in cities such as Cleveland, Chicago, and Washington, D.C.',
+        picture: '[\"http://104.248.156.237:4000/images/ESZww794architecture-buildings-city-1769386.jpg\"]',
         user_id: 4,
+        created_at: new Date(),
+        updated_at: new Date()
+    },
+    {
+        id: 5,
+        title: 'The movement first gained ground in 1893 with the World’s Columbian Exposition in Chicago. Daniel H. Burnham headed the construction of the fair’s temporary city, known to those who attended as the “White City,” a semi-utopia in which visitors were meant to be shielded from poverty and crime. Burnham’s plans for the site incorporated the designs of architects trained at the École des Beaux-Arts in Paris, who paired the balance and harmony of Neoclassical and Baroque architecture with the aesthetic of Chicago’s buildings and cityscape. The landscape of the Columbian Exposition, which included lagoons and big green expanses, was designed by Frederick Law Olmsted, Sr., famous for his winning design of New York City’s oasis, Central Park, which broke ground in 1857. To not only enhance the city’s appearance but also help the flow of vehicle and pedestrian traffic, the City Beautiful concept focused on incorporating a civic centre, parks, and grand boulevards. The holistic and multipurpose approach to urban planning that was championed by Burnham and displayed at the Columbian Exposition remained at the forefront of architecture, landscape architecture, and design for many years. Its impact is still visible in many cities throughout the United States.',
+        picture: '[\"http://104.248.156.237:4000/images/F0yDyaCaarchitecture-buildings-city-325185.jpg\"]',
+        user_id: 5,
+        created_at: new Date(),
+        updated_at: new Date()
+    },
+    {
+        id: 6,
+        title: 'The cat is a small carnivorous mammal. It is the only domesticated species in the family Felidae and often referred to as the domestic cat to distinguish it from wild members of the family. The cat is either a house cat or a farm cat, which are pets, or a feral cat, which ranges freely and avoids human contact.',
+        picture: '[\"http://104.248.156.237:4000/images/m03m2MGDpexels-photo-747795.jpeg\",\"http://104.248.156.237:4000/images/geJh0yBVpexels-photo-1276553.jpeg\", \"http://104.248.156.237:4000/images/jL40ozB9pexels-photo-749212.jpeg\", \"http://104.248.156.237:4000/images/KJjDxol0pexels-photo-2194261.jpeg\"]',
+        user_id: 6,
+        created_at: new Date(),
+        updated_at: new Date()
+    },
+    {
+        id: 7,
+        title: 'Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.',
+        picture: '[\"http://104.248.156.237:4000/images/KXmSzvotmarvel-doctor-strange-wallpaper.jpg\",\"http://104.248.156.237:4000/images/PjoXe9jsWebp.net-resizeimage-4.jpg\",\"http://104.248.156.237:4000/images/qFlnMoLfThe-Avengers-fantasy-warrior-Thor-super-hero-Poster-Ultra-HD-4k-Wallpaper-2880x1800-915x515.jpg\", \"Uu1cf5V8captain-marvel-3090.jpg\",\"http://104.248.156.237:4000/images/y4MSxpvzpapers.co-be92-spiderman-suit-avengers-infinitywar-marvel-hero-art-film-29-wallpaper.jpg\"]',
+        user_id: 7,
+        created_at: new Date(),
+        updated_at: new Date()
+    },
+    {
+        id: 8,
+        title: 'Noun. angel of death (plural angels of death) A personification of death in fiction, art, mythology, or religion. An angel which attends to death, collecting souls at death, ending life at the appointed death. An angel in Abrahamic scripture or eschatology that attends to death.',
+        picture: '[\"http://104.248.156.237:4000/images/szJiPoOjangel-art-black-and-white-208001.jpg\"]',
+        user_id: 8,
         created_at: new Date(),
         updated_at: new Date()
     },
